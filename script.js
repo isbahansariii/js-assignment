075f34e0a2455bd8b39aa31ae35d07afc31bde1c
isbahansariii <140816344+isbahansariii@users.noreply.github.com>
2023-10-14 14:02:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-console.log("Javascript Code");
-console.log("===============")
-let a = 10;         //Variable Declaration and Initizlization
-console.log(a);     //Variable's Value Printing
+console.log("EXTERNAL JAVASCRIPT\n");
+console.log("Javascript Chapter # 1");
+console.log("===============");
+let n = 10;         //Variable Declaration and Initizlization
+console.log(n);     //Variable's Value Printing
 
 /*
     1. Start
@@ -11,10 +12,51 @@ console.log(a);     //Variable's Value Printing
     5. Print the value of 'a'. 
     6. End
 */
-// assigment end
+// Chapter#01 end
 
-// let names = prompt("Enter Your Name: ");
-// console.log("Your name is: %s" , names);
-// alert("Your name is: %s" , names);
-// alert("Hi There!");
-// console.log(Math.random() * 100);
+console.log("\nJavascript Chapter # 2");
+console.log("===============");
+
+console.log("\nPractice 2.1: ");
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log("VARIABLE NAME\tVARIABLE TYPE");
+console.log(str1 + "\t\t\t" + typeof str1);
+console.log(str2 + "\t\t\t\t" + typeof str2);
+console.log(val1 + "\t\t\t" + typeof val1);
+console.log(val2 + "\t\t\t\t" + typeof val2);
+console.log(myNum + "\t\t\t\t" +  typeof myNum);
+
+
+console.log("\nPractice 2.2: ");
+let myName = "Isbah Ansari";
+let age = 20;
+let Javascript_code = true;
+
+console.log("Hello! My name is " + myName + ", I'm " + age + " years old and I can code Javascript: " + Javascript_code + ".");
+
+console.log("\nPractice 2.3: ");
+let base = prompt("Enter base value: ");
+let perpandicular = prompt("Enter perpandicular value: ");
+let hypotenuse = base**2 + perpandicular**2;
+console.log("Hypotenuse of " + base + " and " + perpandicular + " is: " + hypotenuse + ".");
+
+console.log("\nPractice 2.4: ");
+let a=3, b=10, c=29;
+console.log("Before Operation: \ta = " + a + ", b = " + b + " and c = " + c + ".");
+a += b;
+a /= c;
+c %= b;
+console.log("After Operation: \ta = " + a + ", b = " + b + " and c = " + c + ".");
+
+console.log("\nChapter Project:");
+console.log("================");
+let distanceMiles = prompt("Enter distance in miles: ");
+distanceKM = distanceMiles * 1.60934;
+console.log("The distance of " + distanceKM + " kms is equal to " + distanceMiles + " miles.");
+
+// Chapter#02 end
